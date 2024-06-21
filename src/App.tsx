5f1c22c7714e679e0da12f9b9ec6c22094adf203
Asim Nazeer <asimnazeer925@gmail.com>
2024-06-21 15:31:26 +0500

    Do on Click

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import ListGroup from "./components/listGroup";
 
 function App() {
-  return <ListGroup />;
+  const cities = ["karachi", "lahore", "india", "pakistan"];
+  let colors = ["green", "orange"];
+
+  let handleSelected = (item: String) => {
+    console.log(item);
+  };
+
+  return (
+    <>
+      <ListGroup items={cities} heading="cites" onSelectItem={handleSelected} />
+      <ListGroup items={colors} heading="color" onSelectItem={handleSelected} />
+    </>
+  );
 }
 export default App;
--- src/components/listGroup.tsx
+++ src/components/listGroup.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 
-function ListGroup() {
-  const items = ["A", "B", "C"];
+interface Props {
+  items: String[];
+  heading: String;
+  onSelectItem: (item: String) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
   let [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
-      <h1>List</h1>
-      {/* {items.length === 0 ? <p>suorry</p> : null} */}
+      <h1>{heading}</h1>
+      {/* {items.length === 0 ? <p>no item</p> : null} */}
       {items.length === 0 && <p>no item</p>}
-      {/* upper wali line me agr to pehli condition true to return second ! */}
 
       <ul className="list-group">
         {items.map((item, index) => (
@@ -19,7 +23,10 @@ function ListGroup() {
                 ? "list-group-item active"
                 : "list-group-item"
             }
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
