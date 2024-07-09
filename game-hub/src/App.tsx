99e45809c8d932ba9d4e9ff47fdfaac61e91f9cb
Asim Nazeer <asimnazeer925@gmail.com>
2024-07-09 15:34:43 +0500

    Responsive Layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,9 +1,28 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">hell</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            aside
+          </GridItem>
+        </Show>
+
+        <GridItem area="main" bg="dodgerBlue">
+          main
+        </GridItem>
+      </Grid>
     </>
   );
 }
