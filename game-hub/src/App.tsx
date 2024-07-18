fb351d816e1b59aedde0b92b1a643a57a71516df
Asim Nazeer <asimnazeer925@gmail.com>
2024-07-18 12:13:56 +0500

    Displaying Genres

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -11,13 +11,17 @@ function App() {
           base: `"nav" "main"`,
           lg: `"nav nav" "aside main"`,
         }}
+        templateColumns={{
+          base: "1fr",
+          lg: "200px 1fr",
+        }}
       >
         <GridItem area="nav">
           <NavBar />
         </GridItem>
 
         <Show above="lg">
-          <GridItem area="aside">
+          <GridItem area="aside" marginX={5}>
             <GenreList />
           </GridItem>
         </Show>
--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -6,9 +6,9 @@ const GameGrid = () => {
   return (
     <>
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        columns={{ sm: 2, md: 3, lg: 3, xl: 4 }}
         padding="10px"
-        spacing={10}
+        spacing={4}
       >
         {gamesData.map((game) => (
           <GameCard key={game.id} game={game} />
