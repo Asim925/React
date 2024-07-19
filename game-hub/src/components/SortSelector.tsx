90d7e6ee55f6a4edfc5fcd3e13cd1d49a6d405ea
Asim Nazeer <asimnazeer925@gmail.com>
2024-07-19 14:12:46 +0500

    Sort Selector (just button)

--- game-hub/src/components/SortSelector.tsx
+++ game-hub/src/components/SortSelector.tsx
@@ -2,18 +2,23 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
 const SortSelector = () => {
+  let sortOrder = [
+    "Relevance",
+    "Date added",
+    "Name",
+    "Release Date",
+    "Popularity",
+    "Average Rating",
+  ];
   return (
     <Menu>
       <MenuButton marginX={3} as={Button} rightIcon={<BsChevronDown />}>
         Order by: Relevance
       </MenuButton>
       <MenuList>
-        <MenuItem>Relevance</MenuItem>
-        <MenuItem>Date added</MenuItem>
-        <MenuItem>Name</MenuItem>
-        <MenuItem>Release Date</MenuItem>
-        <MenuItem>Popularity</MenuItem>
-        <MenuItem>Average Rating</MenuItem>
+        {sortOrder.map((order) => (
+          <MenuItem key={sortOrder.indexOf(order)}>{order}</MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
